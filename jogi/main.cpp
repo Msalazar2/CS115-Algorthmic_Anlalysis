bca418e1e5085e1963ab6baebd127e9441420755
Yogi <jogishergill@smg-jsvm2q1.auxiliary.local>
2026-03-23 14:11:18 -0700

    Jogi WorkFlow

---
+++ jogi/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "algorithms.h"
+#include "Gen_Input.h"
+#include "test.h"
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
