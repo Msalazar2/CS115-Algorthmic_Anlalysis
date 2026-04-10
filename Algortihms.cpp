baa780e227381a7354c93c1cba340aea5c572e6b
Marcelino Salazar <marcelinosalazar2@gmail.com>
2026-04-10 14:02:52 -0700

    implemented algorithms

--- Algortihms.cpp
+++ Algortihms.cpp
@@ -22,6 +22,65 @@ void SelectionSort(int* numbers, int size) {
     }
 }
 
+// INSERTION SORT
+
+void InsertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+// BUBBLE SORT
+
+void BubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// QUICK SORT
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void QuickSortMain(int arr[], int n) {
+    quickSort(arr, 0, n - 1);
+}
+
 // MERGE SORT
 
 void Merge(int* numbers, int leftFirst, int leftLast, int rightLast) {
